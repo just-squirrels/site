2e07178e184be70672f682f5a8bf059826c377b0
just squirrels <nobody@sqrls.dev>
2023-02-18 17:49:30 -0800

    Switch to static builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
-  output: "server",
-  adapter: node({ mode: "standalone" })
+  output: "static"
 });
